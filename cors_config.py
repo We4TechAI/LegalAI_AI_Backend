3416c743cd15faf56b10e786ed34589459530713
Adarsh Ajay <128365544+achuajays@users.noreply.github.com>
2025-02-19 20:58:05 -0800

    Update cors_config.py

--- cors_config.py
+++ cors_config.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 def configure_cors(app):
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],  # Allow all origins
+        allow_origins=["https://celebrated-rugelach-5b6448.netlify.app/"],  # Allow all origins
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
